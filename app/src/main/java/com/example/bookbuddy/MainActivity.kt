c9b71a855a5794cb92ec6275cf205f0d06330d12
ahjuissi <kinga.zywanska1@gmail.com>
2023-11-10 23:55:29 +0100

    signup layout

--- app/src/main/java/com/example/bookbuddy/MainActivity.kt
+++ app/src/main/java/com/example/bookbuddy/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.bookbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+// import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        //setContentView(R.layout.activity_signup)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment=HomeFragment()
+        val voteFragment=VoteFragment()
+        val searchFragment=SearchFragment()
+        val profileFragment=ProfileFragment()
+
+        setCurrentFragment(homeFragment)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.home->setCurrentFragment(homeFragment)
+                R.id.vote->setCurrentFragment(voteFragment)
+                R.id.search->setCurrentFragment(searchFragment)
+                R.id.profile->setCurrentFragment(profileFragment)
+            }
+            true
+        }
     }
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
 }
