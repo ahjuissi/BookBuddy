5a3b398b6d0f32597cb5162415501e784a8459dc
Patryk <patrykluczak00@gmail.com>
2023-12-06 11:51:56 +0100

    Login i rejestracja gotowe

--- app/src/main/java/com/example/bookbuddy/MainActivity.kt
+++ app/src/main/java/com/example/bookbuddy/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.service.autofill.UserData
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.bookbuddy.databinding.ActivityLoginBinding
 
@@ -11,6 +13,13 @@ import com.example.bookbuddy.databinding.ActivityLoginBinding
 
 import com.example.bookbuddy.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import com.google.firebase.database.ValueEventListener
+import com.google.protobuf.Value
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +27,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bindingLogin: ActivityLoginBinding
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference("Users")
 
         //test NavBar
        // val NavIntent = Intent(this, NavActivity::class.java)
@@ -27,14 +38,42 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
         bindingLogin = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(bindingLogin.root)
 
     //    setContentView(R.layout.activity_login)
-        bindingLogin.btnLoginSubmit.setOnClickListener {}
+        bindingLogin.btnLoginSubmit.setOnClickListener {
+            val email = bindingLogin .etUserName.text.toString() //mail
+            val password = bindingLogin.etPassword.text.toString() // haslo
+            firebase.orderByChild("userEmail").equalTo(email).addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()){
+                        for(userSnapshot in snapshot.children){
+                            val userData= userSnapshot.getValue(UserModel ::class.java)
+
+                            if(userData != null && userData.userPassword == password){
+                                Toast.makeText(this@MainActivity,"Login Successful",Toast.LENGTH_LONG).show()
+                                startActivity(Intent(this@MainActivity, NavActivity::class.java))
+                                finish()
+                            }
+                        }
+                    }
+                    Toast.makeText(this@MainActivity, "Login Failed", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Toast.makeText(this@MainActivity,"Database Error: ${databaseError.message}",Toast.LENGTH_SHORT).show()
+                }
+
+            })
+
+
+        }
         bindingLogin.signupRedirectText.setOnClickListener {
             val signupIntent = Intent(this, SignupActivity::class.java)
             startActivity(signupIntent)
         }
     }
+
 }
--- app/src/main/java/com/example/bookbuddy/SignupActivity.kt
+++ app/src/main/java/com/example/bookbuddy/SignupActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bookbuddy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,8 +17,8 @@ import java.sql.Connection
 
 
 class SignupActivity : AppCompatActivity() {
-    internal var conn: Connection? = null
     private lateinit var bindingSignup: ActivitySignupBinding
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference("Users")
@@ -32,22 +33,34 @@ class SignupActivity : AppCompatActivity() {
         val password2 = bindingSignup.etUserMail.text.toString()
         val city = bindingSignup.spinnerCity.toString()
         val role = bindingSignup.spinnerRole.toString()
+            if(name.isEmpty() || surname.isEmpty() || mail.isEmpty() || password.isEmpty())
+            {
+                Toast.makeText(this, "Please insert all the data", Toast.LENGTH_LONG).show()
+            }else
+            {
+                if(password != password2)
+                {
+                    Toast.makeText(this, "Your passwords must be the same", Toast.LENGTH_LONG).show()
+                }else
+                {
+                    val userId = firebase.push().key!!
 
-            val userId = firebase.push().key!!
+                    val user= UserModel(userId,name,surname,mail,password,1,0)
+                    firebase.child(userId).setValue(user).addOnCompleteListener {
+                        Toast.makeText(this, "Data inserted good", Toast.LENGTH_LONG).show()
+                    }.addOnFailureListener{err->
+                        Toast.makeText(this, "Data insert fail, ${err.message}", Toast.LENGTH_LONG).show()
 
-            val user= UserModel(userId,name,surname,mail,password,1,0)
-            firebase.child(userId).setValue(user).addOnCompleteListener {
-                Toast.makeText(this, "Data inserted good", Toast.LENGTH_LONG).show()
-            }.addOnFailureListener{err->
-                Toast.makeText(this, "Data insert fail, ${err.message}", Toast.LENGTH_LONG).show()
-
+                    }
+                }
             }
 
+
         }
-//        bindingSignup.loginRedirectText.setOnClickListener {
-//            val signupIntent = Intent(this, MainActivity::class.java)
-//            startActivity(signupIntent)
-//        }
+        bindingSignup.loginRedirectText.setOnClickListener {
+            val signupIntent = Intent(this, MainActivity::class.java)
+            startActivity(signupIntent)
+        }
     }
 }
 
