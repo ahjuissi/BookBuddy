ca9b93e5b5a47da9416b16ab09c04c8803c4b6d5
ahjuissi <kinga.zywanska1@gmail.com>
2023-12-09 11:32:33 +0100

    admin_add funkcja nowa showAddView

--- app/src/main/java/com/example/bookbuddy/AdminFragment.kt
+++ app/src/main/java/com/example/bookbuddy/AdminFragment.kt
@@ -15,36 +15,41 @@ import com.example.bookbuddy.databinding.FragmentAdminBinding
 
 class AdminFragment : Fragment(R.layout.fragment_admin) {
     private lateinit var bindingAdmin: FragmentAdminBinding
-//    private lateinit var bindingAdminAdd: FragmentAdminAddBinding
+    private lateinit var bindingAdminAdd: FragmentAdminAddBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        bindingAdmin = FragmentAdminBinding.inflate(layoutInflater)
-//        bindingAdminAdd = FragmentAdminAddBinding.bind(view)
-
-        bindingAdmin.idImgBtnAddUser.setOnClickListener {
-            // Kliknięto przycisk w fragmencie FragmentAdmin
-            println("4")
-//            replaceFragment(R.layout.fragment_admin_add)
-        }
-//        bindingAdminAdd.backBtn.setOnClickListener {
-////            Kliknięto przycisk w fragmencie FragmentAdminAdd
-//            replaceFragment(R.layout.fragment_admin)
-//         }
-
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false)
+        bindingAdmin = FragmentAdminBinding.inflate(layoutInflater)
+        return bindingAdmin.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        bindingAdmin = FragmentAdminBinding.inflate(layoutInflater)
+        bindingAdmin.idImgBtnAddUser.setOnClickListener {
+            // Kliknięto przycisk w fragmencie FragmentAdmin
+            println("4")
+            showAddView()
         }
+    }
+    private fun showAddView() {
+        bindingAdmin = FragmentAdminBinding.inflate(layoutInflater)
+        bindingAdminAdd = FragmentAdminAddBinding.inflate(layoutInflater)
+        bindingAdmin.root.removeAllViews() // Usuń istniejące widoki z widoku "admin"
+        bindingAdmin.root.addView(bindingAdminAdd.root) // Dodaj widok "add"
+
+        // Dodaj obsługę kliknięcia przycisku "Wróć do admin" w widoku "add"
+        bindingAdminAdd.backBtn.setOnClickListener {
+            // Zmiana widoku na "profil admin" bez zmiany fragmentu
+            bindingAdmin.root.removeAllViews() // Usuń istniejące widoki z widoku "add"
+            bindingAdmin.root.addView(bindingAdmin.root) // Dodaj widok "admin" z powrotem
+        }
+    }
 
 
 
