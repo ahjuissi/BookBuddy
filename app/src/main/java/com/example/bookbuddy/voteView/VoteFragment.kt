3f39e46d247f9290af8b2a30acc199cbacc89477
Patryk <patrykluczak00@gmail.com>
2024-01-06 23:58:50 +0100

    Skonczone glosowanie, oraz wybranie wygranej ksiazki

--- app/src/main/java/com/example/bookbuddy/voteView/VoteFragment.kt
+++ app/src/main/java/com/example/bookbuddy/voteView/VoteFragment.kt
@@ -7,11 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.bookbuddy.databinding.FragmentUserVotingBinding
 import com.example.bookbuddy.databinding.FragmentVotingBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -22,22 +21,131 @@ class VoteFragment : Fragment() {
     private lateinit var bindingVoteList: FragmentVotingBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: VotingAdapter
-    lateinit var delete: Button
+    lateinit var delete: FloatingActionButton
     private var firebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var userCity:String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         bindingVoteList = FragmentVotingBinding.inflate(inflater, container, false)
+        delete = bindingVoteList.winner
         return bindingVoteList.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        delete.setOnClickListener {
+            fetchUserCityFromDatabase()
+            checkWinnerTableExistence()
+        }
         setupRecyclerView()
         fetchVoteList()
     }
+    private fun fetchUserCityFromDatabase() {
+        val userId = firebaseAuth.currentUser?.uid
+        userId?.let {
+            val userInfoReference = FirebaseDatabase.getInstance().getReference("userInfo").child(userId)
+            userInfoReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    userCity = snapshot.child("city").getValue(String::class.java).toString()
+                    // Tutaj możesz użyć wartości "userCity" w innych częściach kodu
+                    // Na przykład, przekazać wartość "userCity" do funkcji "saveWinnerInfoToDatabase"
+                    // lub gdziekolwiek indziej jest potrzebna wartość "city"
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.e("VoteFragment", "Error fetching user city: ", databaseError.toException())
+                }
+            })
+        }
+    }
+    private fun checkWinnerTableExistence() {
+        val databaseReference = FirebaseDatabase.getInstance().getReference("Winner")
+
+        databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (!snapshot.exists()) {
+                    Log.d("VoteFragment", "Winner table does not exist.")
+                    val votingReference = FirebaseDatabase.getInstance().getReference("Voting")
+
+                    votingReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(votingSnapshot: DataSnapshot) {
+                            var maxLikes = 0
+                            var winnerBookId = ""
+                            val userId = firebaseAuth.currentUser?.uid ?: ""
+                            val winnerData = mutableListOf<WinnerInfo>()
+
+                            for (votingChildSnapshot in votingSnapshot.children) {
+                                val bookId = votingChildSnapshot.child("id").getValue(String::class.java)
+                                var totalLikes = 0
+                                var totalDislikes = 0
+
+                                votingChildSnapshot.child("bookLikes").getValue(Int::class.java)?.let {
+                                    totalLikes += it
+                                }
+
+                                votingChildSnapshot.child("bookDislikes").getValue(Int::class.java)?.let {
+                                    totalDislikes += it
+                                }
+
+                                if (totalLikes > maxLikes) {
+                                    maxLikes = totalLikes
+                                    winnerBookId = bookId ?: ""
+                                }
+                            }
+
+                            winnerBookId.takeIf { it.isNotEmpty() }?.let {
+                                val winner = WinnerInfo(userId, userCity, it, maxLikes)
+                                winnerData.add(winner)
+                                saveWinnerInfoToDatabase(winnerData)
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+                            Log.w(ContentValues.TAG, "Error getting documents: ", databaseError.toException())
+                        }
+                    })
+                } else {
+                    databaseReference.removeValue()
+                        .addOnSuccessListener {
+                            Log.d("VoteFragment", "Winner table deleted successfully.")
+                            // Tutaj możesz dodać kod, który ma być wykonany po usunięciu tabeli "Winner"
+                        }
+                        .addOnFailureListener { e ->
+                            Log.e("VoteFragment", "Error deleting Winner table: $e")
+                        }
+                }
+                // Tutaj możesz dodać kod, który ma być wykonany bez względu na istnienie tabeli Winner
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("VoteFragment", "Error checking Winner table existence: ", databaseError.toException())
+            }
+        })
+    }
+
+    // Funkcja zapisująca dane do tabeli Winner dla każdej książki z osobna
+    private fun saveWinnerInfoToDatabase(winnerData: List<WinnerInfo>) {
+        for (winner in winnerData) {
+            val userId = winner.userId
+            val bookId = winner.bookId
+            val totalLikes = winner.totalVotes
+            val city = winner.city
+
+            val winnerReference = FirebaseDatabase.getInstance().getReference("Winner").child(bookId.toString())
+
+            winnerReference.setValue(winner)
+                .addOnSuccessListener {
+                    Log.d("VoteFragment", "Winner info for book $bookId saved successfully.")
+                }
+                .addOnFailureListener { e ->
+                    Log.e("VoteFragment", "Error saving winner info for book $bookId: $e")
+                }
+        }
+    }
+
 
     private fun setupRecyclerView() {
         recyclerView = bindingVoteList.recyclerView
---
+++ app/src/main/java/com/example/bookbuddy/voteView/WinnerInfo.kt
@@ -0,0 +1,8 @@
+package com.example.bookbuddy.voteView
+
+data class WinnerInfo(
+    val userId: String = "",
+    val city: String = "",
+    val bookId: String="",
+    val totalVotes: Any
+)
