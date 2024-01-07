23ddb16a7d08d981e39d97ce177544eabe73ba09
Patryk <patrykluczak00@gmail.com>
2024-01-07 23:55:30 +0100

    prawie koniec

--- app/src/main/java/com/example/bookbuddy/homeView/ModelComment.kt
+++ app/src/main/java/com/example/bookbuddy/homeView/ModelComment.kt
@@ -1,11 +1,11 @@
 package com.example.bookbuddy.homeView
 
 class ModelComment(
-    var cId: String?,
-    var comment: String?,
-    var ptime: String?,
-    var udp: String?,
-    var uemail: String?,
-    var uid: String?,
-    var uname: String?
+    var cId: String?="",
+    var comment: String?="",
+    var ptime: String?="",
+    var udp: String?="",
+    var uemail: String?="",
+    var uid: String?="",
+    var uname: String?=""
 ) {}
